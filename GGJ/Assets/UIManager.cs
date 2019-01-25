2f2625e709f89bb1d10b9813694716635dca9cd8
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2019-01-26 01:24:29 +0800

    19.1.26.1:24

---
+++ GGJ/Assets/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Image BackGround_001;   //背景图_1
+    public Text Texts;             //文本_1
+    public Button Button_001;     //按钮_1
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

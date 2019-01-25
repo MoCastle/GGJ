10c7c5b1f95acf6a5e5c4d6fa57fe45b13423675
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2019-01-26 01:25:49 +0800

    Update UIManager.cs

--- GGJ/Assets/UIManager.cs
+++ GGJ/Assets/UIManager.cs
@@ -5,20 +5,48 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public Image BackGround_001;   //背景图_1
-    public Text Texts;             //文本_1
-    public Button Button_001;     //按钮_1
-    
-    
-    // Start is called before the first frame update
-    void Start()
+   
+    public GameObject Animation_001;     //动画预设体
+    public GameObject Button_001;    //按钮预设体
+    public Image BackGround_001;   //背景图1
+    public Text Texts_001;             //文本1
+
+    private bool flag;         //是否播放完
+
+    private int number;          //每一行所显示的字数
+
+
+    /// <summary>
+    /// 控制显示哪段文本
+    /// </summary>
+    public void Which_Text(int id) 
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 控制显示哪个动画
+    /// </summary>
+    /// <param name="id"></param>
+    public void Which_Animation(int id)
     {
-        
     }
+
+    /// <summary>
+    /// 控制显示哪张背景
+    /// </summary>
+    /// <param name="id"></param>
+    public void Which_BG(int id)
+    {
+    }
+
+    /// <summary>
+    /// 点击事件
+    /// </summary>
+    /// <param name="button"></param>
+    public void OnClick(GameObject button)
+    {
+    }
+    }
+
 }

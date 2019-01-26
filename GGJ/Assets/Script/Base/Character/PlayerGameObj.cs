e28dc8a972e80664d7147a3e37914a1b8c7980ce
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2019-01-26 22:33:27 +0800

    19.1.26

--- GGJ/Assets/Script/Base/Character/PlayerGameObj.cs
+++ GGJ/Assets/Script/Base/Character/PlayerGameObj.cs
@@ -25,38 +25,38 @@ public class PlayerGameObj : MonoBehaviour
     public void Move( int forward)
     {
 
-        GameMap map = GameMgr.Mgr.Map;
-        InputListener.isMove = false;
+           GameMap map = GameMgr.Mgr.Map;
+           InputListener.isMove = false;
 
-        Location location = GameMgr.Mgr.Map.GetLocationByIdx(CurFloorIdx);
+         //    Location location = GameMgr.Mgr.Map.GetLocationByIdx(CurFloorIdx);
 
-        Vector3 oldPS = transform.position;
+              Vector3 oldPS = transform.position;
 
-        Debug.Log("oldPS is " + oldPS);
-        switch (forward)
-        {
-            //上
-            case 1:
-                location.y += 1;
-                break;
-            //左
-            case 2:
-                location.x -= 1;
-                break;
-            //下
-            case 3:
-                location.y -= 1;
-                break;
-            //右
-            case 4:
-                location.x += 1;
-                break;
-            default:
-                break;
-        };
-        int newIdx = map.GetIdxByLocation(location);
-        CurFloorIdx = newIdx;
+              Debug.Log("oldPS is " + oldPS);
+        /*    switch (forward)
+            {
+                //上
+                case 1:
+                    location.y += 1;
+                    break;
+                //左
+                case 2:
+                    location.x -= 1;
+                    break;
+                //下
+                case 3:
+                    location.y -= 1;
+                    break;
+                //右
+                case 4:
+                    location.x += 1;
+                    break;
+                default:
+                    break;
+            };
+         //   int newIdx = map.GetIdxByLocation(location);
+            CurFloorIdx = newIdx;
         
-        transform.position = map.GetPosition(newIdx);
+          transform.position = map.GetPosition(newIdx); */
     }
 }

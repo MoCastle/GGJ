c541b707735a50484122d833689738bc6c6daa1d
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2019-01-27 00:46:27 +0800

    19.1.27

--- GGJ/Assets/Script/UI/UIManager.cs
+++ GGJ/Assets/Script/UI/UIManager.cs
@@ -15,16 +15,26 @@ public class UIManager : MonoBehaviour
     public GameObject TextBox;       //对话框组件
 
     public GameObject Button_001;
-    public GameObject Button_002;
-    public GameObject Button_003;
-    public GameObject Button_004;
+    public GameObject Exit;
+    public GameObject ReStart;
+    public GameObject Story;
+
+    public GameObject Next;
+    public GameObject Return;
+
+    public GameObject Vague;//模糊遮罩
+
+    public GameObject StoryBook;
+    public Text All_Story;
+    private int number_Story = 40;
+    private int thisChar_Story = 0;
 
     private string All_Tex="一二三四五,!六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十十一十二";
 
     private bool Flag_Text=true;         //是否播放完文本
 
-    private int number=8;          //每一行所显示的字数
-    private int ThisChar = 0;      //当前显示到第几个字符
+    private int number_Conver=8;          //每一行所显示的字数
+    private int ThisChar_Conver = 0;      //当前显示到第几个字符
 
     public string all_Tex 
     {
@@ -36,12 +46,17 @@ public class UIManager : MonoBehaviour
         //按钮事件注册
         if (Button_001!= null)
         EventListener.Get(Button_001.GetComponent<Button>().gameObject).onClick = MyOnClick;
-        if (Button_002!=null)
-        EventListener.Get(Button_002.GetComponent<Button>().gameObject).onClick = MyOnClick;
-        if (Button_003 != null)
-            EventListener.Get(Button_003.GetComponent<Button>().gameObject).onClick = MyOnClick;
-        if (Button_004 != null)
-            EventListener.Get(Button_004.GetComponent<Button>().gameObject).onClick = MyOnClick;
+        if (Exit != null)
+            EventListener.Get(Exit.GetComponent<Button>().gameObject).onClick = MyOnClick;
+        if (ReStart != null)
+            EventListener.Get(ReStart.GetComponent<Button>().gameObject).onClick = MyOnClick;
+        if (Story != null)
+            EventListener.Get(Story.GetComponent<Button>().gameObject).onClick = MyOnClick;
+
+        EventListener.Get(Next.gameObject).onClick = MyOnClick;
+        EventListener.Get(Return.gameObject).onClick = MyOnClick;
+
+        StoryBook.transform.DOScale(0, 0);
      //   TextBox.transform.DOScaleX(0, 0f);//对话框初始化
     //    Which_Text(1);
     }
@@ -65,29 +80,39 @@ public class UIManager : MonoBehaviour
         if (Button_001 != null && button ==Button_001.GetComponent<Button>().gameObject)
         {
             thisScene = 1;
-    
             SceneManager.LoadScene("Scene1");
             Debug.Log("start");
            // SceneManager.LoadScene("scene2");
         }
-        if (Button_002!=null&&button == Button_002.GetComponent<Button>().gameObject)
+        if (Exit != null && button == Exit.GetComponent<Button>().gameObject)
         {
             Debug.Log("Exit");
             // SceneManager.LoadScene("scene2");
         }
-        if (Button_003 != null && button == Button_003.GetComponent<Button>().gameObject)
+        if (ReStart != null && button == ReStart.GetComponent<Button>().gameObject)
         {
-           // thisScene += 1;
-         //   Debug.Log("Scene" + thisScene.ToString());
-          //  SceneManager.LoadScene("Scene"+thisScene.ToString());
-            // SceneManager.LoadScene("scene2");
-        }
-        if (Button_004 != null && button == Button_004.GetComponent<Button>().gameObject)
-        {
+            Debug.Log("ReStart");
             thisScene += 1;
             Debug.Log("Scene" + thisScene.ToString());
-            SceneManager.LoadScene("Scene" + thisScene.ToString());
-           // Debug.Log("Story");            // 
+          SceneManager.LoadScene("Scene"+thisScene.ToString());
+            // SceneManager.LoadScene("scene2");
+        }
+        if (Story != null && button == Story.GetComponent<Button>().gameObject)
+        {
+         //   thisScene += 1;
+          //  Debug.Log("Scene" + thisScene.ToString());
+          //  SceneManager.LoadScene("Scene" + thisScene.ToString());
+            StoryBook.transform.DOScale(1, 0.3f);
+            Debug.Log("Story");            // 
+        }
+        if (button == Next.gameObject)
+        {
+            Debug.Log("Next");
+        }
+        if (button ==Return.gameObject)
+        {
+            StoryBook.transform.DOScale(0, 0.4f);
+            Debug.Log("Return");
         }
       /*  if (button = Button_002.GetComponent<Button>().gameObject)
         {
@@ -104,24 +129,21 @@ public class UIManager : MonoBehaviour
     /// </summary>
     public void Which_Text(int id)
     {
-
         if (Flag_Text)//若文本未显示完
         {
             Texts_001.GetComponent<Text>().text = null;//清空对话框中文字
-            for (int i = 0; i < number; i++)
+            for (int i = 0; i < number_Conver; i++)
             {
-                
                 try
                 {
-                    Texts_001.GetComponent<Text>().text += All_Tex[ThisChar + i];//添加文字
+                    Texts_001.GetComponent<Text>().text += All_Tex[ThisChar_Conver + i];//添加文字
                 }
                 catch
                 {
                     Flag_Text = false;
-
                 }
             }
-            ThisChar = ThisChar + number;      //记录文本下标
+            ThisChar_Conver = ThisChar_Conver + number_Conver;      //记录文本下标
         }
         else
         {

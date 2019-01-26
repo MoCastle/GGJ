9a9ba9b70f07214fa76272fd2362ca260bfbf1dd
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2019-01-26 22:44:13 +0800

    19.1.26.22:44

---
+++ GGJ/Assets/Script/Scene/SpiteManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiteManager : MonoBehaviour
+{
+    public Sprite[] Scene_001;
+    public Sprite[] Scene_002;
+    public Sprite[] Scene_003;
+    public Sprite[] Scene_004;
+}

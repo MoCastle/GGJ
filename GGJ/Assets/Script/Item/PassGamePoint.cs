9f85d5a7a070b5d5a529bbd1cea4dac1f28230a3
MoCastle <38985297+MoCastle@users.noreply.github.com>
2019-01-26 13:11:50 +0800

    道具

---
+++ GGJ/Assets/Script/Item/PassGamePoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PassGamePoint : Item
+{
+    [Title("下一关名字", "black")]
+    public string NextSceneName;
+    [Title("兄弟过关点", "black")]
+    public PassGamePoint Brother;
+    bool _IsPlayerTouched;
+    public bool ISPlayerTouched
+    {
+        get
+        {
+            return _IsPlayerTouched;
+        }
+    }
+    public void Touched()
+    {
+        _IsPlayerTouched = true;
+        if( !Brother.ISPlayerTouched )
+        {
+            return;
+        }
+        if(NextSceneName!="")
+            SceneManager.LoadScene(NextSceneName);
+    }
+}

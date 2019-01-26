003e3a700a0d3f10cb362559d1bdde434f8a04ed
MoCastle <38985297+MoCastle@users.noreply.github.com>
2019-01-26 10:45:50 +0800

    增加游戏控制功能

---
+++ GGJ/Assets/Script/Base/BaseFrameObj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseFrameObj : MonoBehaviour
+{
+     public BaseFrame BaseFrame
+    {
+        get;
+        set;
+    }
+    private void Update()
+    {
+        if(BaseFrame!=null)
+        {
+            BaseFrame.Update();
+        }
+    }
+}
